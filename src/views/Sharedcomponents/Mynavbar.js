578ddafce07d41ceb2ccd1e7b790283ddbe23126
johdeza <johdeza261093@gmail.com>
2020-11-26 01:18:33 -0600

    ya sirve navbar con react-bootstrap

--- src/views/Sharedcomponents/Mynavbar.js
+++ src/views/Sharedcomponents/Mynavbar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Navbar, Nav } from "react-bootstrap";
 
 const Mynavbar = () => {
   const [isOpen, setisOpen] = React.useState(false);
@@ -8,45 +9,17 @@ const Mynavbar = () => {
 
   return (
     <div>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div className="container">
-          <a className="navbar-brand" href="#/inicio">
-            Nutrición
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse m-auto" id="navbarNav">
-            <ul className="navbar-nav ml-auto">
-              <li className="nav-item">
-                <a className="nav-link" href="#/inicio">
-                  Inicio
-                  <span className="sr-only">(current)</span>
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#/login">
-                  Login
-                </a>
-              </li>
-
-              <li className="nav-item">
-                <a className="nav-link" href="#/login">
-                  Nueva cuenta
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar.Brand href="#inicio">Nutrición</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="ml-auto">
+            <Nav.Link href="#inicio">Inicio</Nav.Link>
+            <Nav.Link href="#login">Login</Nav.Link>
+            <Nav.Link href="#login">Nueva cuenta</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     </div>
   );
 };
