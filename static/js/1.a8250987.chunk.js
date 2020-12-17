a8f2a13edfa52556a4f389264691357cce546ef2
johdeza <johdeza261093@gmail.com>
2020-12-16 20:03:19 -0600

    Updates

---
+++ static/js/1.a8250987.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@coreui/coreui-free-react-admin-template"]=this["webpackJsonp@coreui/coreui-free-react-admin-template"]||[]).push([[1],{630:function(e,c,i){}}]);
+//# sourceMappingURL=1.a8250987.chunk.js.map
